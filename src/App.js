90636be1d68b7da022cdd8750c169bc8a4087c57
Bipul0311 <vk1793869@gmail.com>
2021-02-11 21:53:26 +0530

    Next Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Navbar from './components/pages/layout/Navbar';
+import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NotFound from './components/pages/NotFound';
+import AddUser from './components/pages/users/AddUser';
+import EditUser from './components/pages/users/EditUser';
+import User from './components/pages/users/User';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Router>
+        <div className="App">
+            
+                <Navbar />
+
+                <Switch>
+                    <Route exact path = "/" component={Home}/>
+                    <Route exact path = "/About" component={About}/>
+                    <Route exact path = "/Contact" component={Contact}/>
+                    <Route exact path = "/users/add" component={AddUser}/>
+                    <Route exact path = "/users/edit/:id" component={EditUser}/>
+                    <Route exact path = "/users/:id" component={User}/>
+                    <Route component={NotFound}/>
+                    
+
+                </Switch>
+        </div>
+        </Router>
+    )
 }
 
 export default App;
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,29 @@
+import React from'react';
+
+const About = () => {
+    return(
+         <div className="container">
+             <div className="py-4">
+                  <h2>About page</h2>
+                  <p className="lead">
+                    Apple Inc. is an American multinational technology company headquartered in Cupertino, California,
+                    that designs, develops, and sells consumer electronics, computer software, and online services.
+                  </p>
+
+                  <p className="lead">
+                    Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services.
+                  </p>
+
+                  <p className="lead">
+                    Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services.
+                  </p>
+
+                  <p className="lead">
+                    Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services.
+                  </p>
+             </div>
+         </div>
+    );
+}
+
+export default About;
